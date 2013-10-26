9be803d0d45fbefe35ca1e81acc484f70990bf2f
dridri <dridri85@gmail.com>
2013-10-26 16:41:48 +0200

    corrected CMakeFiles

--- src/video/opengl30/gedraw3d.c
+++ src/video/opengl30/gedraw3d.c
@@ -18,6 +18,10 @@
 
 #include "../../ge_internal.h"
 
+#ifndef max
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+#endif
+
 static ge_Scene* current_scene;
 extern ge_Camera* ge_current_camera;
 extern u32 shadow_map;
